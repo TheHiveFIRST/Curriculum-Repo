5a060a0b0485f934eb49a327ebce6f6b6d03f3e2
Boppitystar <stephaniechang966@gmail.com>
2025-09-07 16:15:52 -0600

    robot changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
   private final StingerSubsystem m_StingerSubsystem = new StingerSubsystem();
   private final ElevatorSubsystem m_ElevatorSubsystem = new ElevatorSubsystem();
   private final LEDSubsystem m_LedSubsystem = new LEDSubsystem();
-  // private final DriveSubsystem m_robotDrive = new DriveSubsystem();  // Ensuring DriveSubsystem is properly initialized
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();  // Ensuring DriveSubsystem is properly initialized
 
   // Variables
   private double setPos = 0;
@@ -78,34 +78,8 @@ public class Robot extends TimedRobot {
 
   /** Runs the autonomous sequence */
   @Override
-  public void autonomousPeriodic() {
-    // System.out.println("Autonomous Timer: " + m_timer.get());  // Debug log
+  public void autonomousPeriodic() {}
 
-    // if (m_timer.get() < 3.0) {
-    //   m_robotDrive.drive(0, 0.5, 0, true);
-    //   System.out.println("Moving Forward...");
-    // } else {
-    //   m_robotDrive.drive(0, 0, 0, true); // Stop movement
-    //   System.out.println("Stopping...");
-
-    //   if (m_timer.get() > 3.0) {
-    //     m_StingerSubsystem.setIntakePower(0.5);
-    //     delayTimer(3);
-    //     m_timer.stop();
-    //   }
-    // }
-  }
-
-  /** Schedules a path-following command */
-  // public void runPath() {
-  //   Command pathCommand = new PathPlannerAuto("Ishana Path");
-  //   if (pathCommand != null) {
-  //     pathCommand.schedule();
-  //     System.out.println("Running Path: Ishana Path");
-  //   } else {
-  //     System.out.println("Failed to schedule Path");
-  //   }
-  // }
 
   @Override
   public void teleopInit() {
@@ -174,6 +148,6 @@ public class Robot extends TimedRobot {
 
   /** Delays execution for a set time */
   public void delayTimer(int seconds) {
-    m_timer.delay(seconds);
+    Timer.delay(seconds);
   }
 }
